252c047b011264551f4f84ee196579b7d92debb1
meanly <63057881+meanly@users.noreply.github.com>
2022-12-07 15:05:20 +0800

    optimize
    trianglebutton

--- Scripts/Controls/TriangleButton.cs
+++ Scripts/Controls/TriangleButton.cs
@@ -2,53 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriangleButton : MonoBehaviour
-{
+public class TriangleButton: MonoBehaviour {
 
-/////PENDING WORK: IMPLEMENTING THIS BUTTON FOR RUNNING
-    CharacterAnimator characterAnimator;
-    bool IsRunning;
-    bool StopRunning;
+  /////PENDING WORK: IMPLEMENTING THIS BUTTON FOR RUNNING
+  CharacterAnimator characterAnimator;
+  bool IsRunning;
+  bool StopRunning;
 
-    private void start()
-    {
-        characterAnimator = GetComponent<CharacterAnimator>();
-    }
+  private void start() {
+    characterAnimator = GetComponent < CharacterAnimator > ();
+  }
 
-    public void pointerDown()
-    {
-        StopRunning = false;
-        Invoke("RunVariableTrue", 0.5f);
-    }
-    
-    public void pointerUp()
-    {
-        IsRunning = false;
-        StopRunning = true;
-    }
+  public void pointerDown() {
+    StopRunning = false;
+    Invoke("RunVariableTrue", 0.5f);
+  }
 
-    void RunVariableTrue()
-    {
-        IsRunning = true;
-    }
+  public void pointerUp() {
+    IsRunning = false;
+    StopRunning = true;
+  }
 
-    void RunVariableFalse()
-    {
-        IsRunning = false;
-        if (StopRunning == false)
-        {
-            
-            Invoke("RunVariableTrue", 0.5f);
-        }
-    }
+  void RunVariableTrue() {
+    IsRunning = true;
+  }
 
-    void Update ()
-    {
-        if(IsRunning)
-        {
-            RunVariableFalse();
-            
-            Debug.Log("The player is now running!");  //i need to change the value of the movementspeed and animation in this block
-        }
+  void RunVariableFalse() {
+    IsRunning = false;
+    if (StopRunning == false) {
+      Invoke("RunVariableTrue", 0.5f);
     }
+  }
+
+  void Update() {
+    if (IsRunning) {
+      RunVariableFalse();
+
+      Debug.Log("The player is now running!"); //i need to change the value of the movementspeed and animation in this block
+    }
+  }
 }
