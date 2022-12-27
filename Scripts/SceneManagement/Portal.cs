2e6bd3883d82b1c6c0e63c674be78631d565abdd
meanly <63057881+meanly@users.noreply.github.com>
2022-12-27 18:16:10 +0800

    fader

--- Scripts/SceneManagement/Portal.cs
+++ Scripts/SceneManagement/Portal.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class Portal : MonoBehaviour, IPlayerTriggerable
 {
@@ -19,17 +19,26 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
         StartCoroutine(SwitchScene());
     }
 
+    Fader fader;
+    private void Start() {  
+        fader =  FindObjectOfType<Fader>();
+    }
+
     IEnumerator SwitchScene()
     {
         DontDestroyOnLoad(gameObject);
+        //start of script and fading
 
         GameController.Instance.PauseGame(true);
+        yield return fader.FadeIn(0.5f);
 
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
         var destPortal = FindObjectsOfType<Portal>().First(x => x != this && x.destinationPortal == this.destinationPortal);
         player.Character.SetPositionandSnapToTile(destPortal.SpawnPoint.position);
 
+        //end
+        yield return fader.FadeOut(0.5f);
         GameController.Instance.PauseGame(false);
 
         Destroy(gameObject);
