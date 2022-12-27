f683a4dfb18df8cb6875a4107bf4ac8e77b575f7
meanly <63057881+meanly@users.noreply.github.com>
2022-12-27 14:48:15 +0800

    portal

--- Scripts/SceneManagement/Portal.cs
+++ Scripts/SceneManagement/Portal.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Portal : MonoBehaviour, IPlayerTriggerable
 {
+    [SerializeField] int sceneToLoad = -1;
+
     public void OnPlayerTriggered(PlayerController player)
     {
         Debug.Log("Player entered the Portal");
+        StartCoroutine(SwitchScene());
+    }
+
+    IEnumerator SwitchScene()
+    {
+        yield return SceneManager.LoadSceneAsync(sceneToLoad);
     }
 }
