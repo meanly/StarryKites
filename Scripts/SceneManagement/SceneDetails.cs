63f6e03d4d546af87d340346021c273918827ede
meanly <63057881+meanly@users.noreply.github.com>
2022-12-30 14:24:58 +0800

    scenedetails

--- Scripts/SceneManagement/SceneDetails.cs
+++ Scripts/SceneManagement/SceneDetails.cs
@@ -1,14 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneDetails : MonoBehaviour
 {
+    [SerializeField] List<SceneDetails> connectedScenes;
+
+    public bool IsLoaded { get; private set; }
     private void OnTriggerEnter2D(Collider2D collision) 
     {
         if (collision.tag == "Player");
         {
-            Debug.Log($"Entered{gameObject.name}");
+            Debug.Log($"Entered {gameObject.name}");
+            LoadScene();
+
+            //load all connected scenes
+            foreach (var scene in connectedScenes)
+            {
+                scene.LoadScene();
+            }
+        }
+    }
+
+    public void LoadScene()
+    {
+        if (!IsLoaded)
+        {
+            SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive);
+            IsLoaded = true;
         }
     }
 }
