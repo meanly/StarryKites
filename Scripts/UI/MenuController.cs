e264ca10754be4ddf3cb4f28d1383d654bf5f181
meanly <63057881+meanly@users.noreply.github.com>
2023-01-05 17:10:39 +0800

    Update MenuController.cs

--- Scripts/UI/MenuController.cs
+++ Scripts/UI/MenuController.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject menu;
+    public void OpenMenu()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        menu.SetActive(true);
     }
 }
